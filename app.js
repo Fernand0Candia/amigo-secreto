40579ed82d1e06e355e8cae7e01d0f01fd9fcf77
Fernando <fernandocandia2002@gmail.com>
2025-03-17 15:09:22 -0600

    Actualización de app.js

--- app.js
+++ app.js
@@ -1,28 +1,30 @@
-//Creacion de array
+//Creación de array
 let amigos = []
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//Función para agregar amigos
 function agregarAmigo() {
-    document.getElementById ('amigo');
+    const input = document.getElementById ('amigo');
+    const nombre = input.value.trim();
+
+    //Validar que el campo no este vacio
+    if (nombre === '') {
+        alert('Nombre no valido');
+        return;
+    }
+
+    //Agrega los nombres al array
+    amigos.push(nombre);
+
+    //Actualiza la lista
+    actualizarLista();
+
+    //Limpia el campo luego de añadir un nombre 
+    input.value = '';
 }
 
+//Función para actualizar la lista de nombres 
+function actualizarLista() {
+    const listaAmigos = document.getElementById('listaAmigos');
 
-
-
-
+    
+}
